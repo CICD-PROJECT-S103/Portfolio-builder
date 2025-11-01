4c4ac472f03a63a1f3bca4f59ca9518211e1498a
Guru2006ug <gurushaileshmopuri@gmail.com>
2025-11-01 19:07:27 +0530

    Added logout module

--- portfoliobackend-project/src/main/java/com/klu/backend/controller/CustomerController.java
+++ portfoliobackend-project/src/main/java/com/klu/backend/controller/CustomerController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.klu.backend.model.Customer;
 import com.klu.backend.model.CustomerManager;
+import com.klu.backend.model.TokenHandle;
 
 
 
@@ -19,7 +21,9 @@ import com.klu.backend.model.CustomerManager;
 public class CustomerController {
 	@Autowired
     CustomerManager UM;
-
+	@Autowired
+    TokenHandle jwt;
+	
     @PostMapping("/signup")
     public String signup(@RequestBody Customer U) {
         return UM.addUSer(U);
@@ -30,4 +34,11 @@ public class CustomerController {
 		return UM.CheckCredentials(U.getEmail(), U.getPassword());
 		
 	}
+    @PostMapping("/logout")
+    public String logout(@RequestHeader("Authorization") String token) {
+        if (token.startsWith("Bearer ")) {
+            token = token.substring(7);
+        }
+        return jwt.logoutToken(token);
+    }
 }
